3791f28af837a173a474f6a65b6e7399894d4952
cdmo <cdmorris22@gmail.com>
2015-08-14 15:10:52 -0400

    no-cache!

--- drushrc.php
+++ drushrc.php
@@ -5,8 +5,8 @@
  * like `drush @yoursite.local pull-live-all`
  */
 $options['shell-aliases']['pull-stage-files'] = 'rsync -v {{#stage}}:%files {{@target}}:%files';
-$options['shell-aliases']['pull-stage-data'] = 'sql-sync -v {{#stage}} {{@target}} && drush golocal';
-$options['shell-aliases']['pull-stage-all'] = '!drush sql-sync -v {{#stage}} {{@target}} && drush rsync -v {{#stage}}:%files {{@target}}:%files && drush golocal';
+$options['shell-aliases']['pull-stage-data'] = 'sql-sync -v --no-cache {{#stage}} {{@target}} && drush golocal';
+$options['shell-aliases']['pull-stage-all'] = '!drush sql-sync -v --no-cache {{#stage}} {{@target}} && drush rsync -v {{#stage}}:%files {{@target}}:%files && drush golocal';
 $options['shell-aliases']['pull-prod-files'] = 'rsync -v {{#prod}}:%files {{@target}}:%files';
-$options['shell-aliases']['pull-prod-data'] = '!drush sql-sync -v {{#prod}} {{@target}} --no-cache  && drush golocal';
-$options['shell-aliases']['pull-prod-all'] = '!drush sql-sync -v {{#prod}} {{@target}} --no-cache && drush rsync -v {{#prod}}:%files {{@target}}:%files && drush golocal';
+$options['shell-aliases']['pull-prod-data'] = '!drush sql-sync -v --no-cache {{#prod}} {{@target}} --no-cache  && drush golocal';
+$options['shell-aliases']['pull-prod-all'] = '!drush sql-sync -v --no-cache {{#prod}} {{@target}} --no-cache && drush rsync -v {{#prod}}:%files {{@target}}:%files && drush golocal';
